e0b8ba916855ac30ee325ab4617afc3f5322edd8
Elena <elenademonte@libero.it>
2018-05-03 15:37:25 +0200

    Esercizio 7 risolto

---
+++ homework1/esercizio7.js
@@ -0,0 +1,15 @@
+function FunctionMouseOver() {
+    window.alert("Errore Mouse Over")
+}
+
+function FunctionMouseOut() {
+    window.alert("Errore Mouse Out")
+}
+
+function FunctionClick() {
+    window.alert("Errore Click")
+}
+
+function FunctionContextMenu() {
+    window.alert("Errore ContextMenu")
+}
